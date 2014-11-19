faad684444910655658cd860fc5bbbdf8094fa0f
xiangacadia <xiangjiang.acadia@gmail.com>
2014-11-18 22:23:47 -0400

    verified GD is better than ZI

--- simulation.R
+++ simulation.R
@@ -12,8 +12,8 @@ add2OrderBook <- function(id, price)
     # sort order book
     orderBook <- orderBook[order(orderBook$price),]
     
-    cat("The current order book is:\n")
-    print(orderBook)
+    # cat("The current order book is:\n")
+    # print(orderBook)
     return(orderBook)
 }
 
@@ -27,7 +27,7 @@ getMaxPrice <- function()
 # bid action
 bid <- function(iteration, id, price)
 {
-    cat(sprintf("The current bidding price for agent %d is: %f\n", id, price))
+    # cat(sprintf("The current bidding price for agent %d is: %f\n", id, price))
     
     # add current bid to order book
     orderBook <<- add2OrderBook(id, price)
@@ -81,6 +81,22 @@ readSimulationData <- function()
     x = load("agent2.Rdata")
     agent2Acc <<- get(x[3])
     agent2Unacc <<- get(x[4])
+    
+    x = load("agent3.Rdata")
+    agent3Acc <<- get(x[3])
+    agent3Unacc <<- get(x[4])
+    
+    x = load("agent4.Rdata")
+    agent4Acc <<- get(x[3])
+    agent4Unacc <<- get(x[4])
+    
+    x = load("agent5.Rdata")
+    agent5Acc <<- get(x[3])
+    agent5Unacc <<- get(x[4])
+    
+    x = load("agent6.Rdata")
+    agent6Acc <<- get(x[3])
+    agent6Unacc <<- get(x[4])
 }
 
 # bidding strategy for agent 1
@@ -97,14 +113,20 @@ agentGD <- function(id)
             p <- getP(price, agent2Acc, agent2Unacc)
         if (id == 3)
             p <- getP(price, agent3Acc, agent3Unacc)
+        if (id == 4)
+            p <- getP(price, agent4Acc, agent4Unacc)
+        if (id == 5)
+            p <- getP(price, agent5Acc, agent5Unacc)
+        if (id == 6)
+            p <- getP(price, agent6Acc, agent6Unacc)
         
-        if (p > 0.7)
-            p = p - 0.3
+        if (p > 0.6)
+            p = p - 0.35
         
         temp <- rep(price, as.integer(p * 100))
         pricePool <- c(pricePool, temp)
     }
-
+    
     return (sample(pricePool, 1))
 }
 
@@ -115,6 +137,10 @@ getP <- function(price, faccepted, funaccepted)
     abl <- length(faccepted[faccepted < price])
     ubg <- length(funaccepted[funaccepted > price])
     p <- abl / (abl + ubg)
+    if (p > 0.7)
+        p = p - 0.3
+    
+    p <- p / (price + 45) * 500
     return(p)
 }
 
@@ -126,28 +152,58 @@ unaccepted <<- c()
 agentHistory1 <<- data.frame(matrix(vector(), 0, 3, dimnames=list(c(), c("iteration", "price", "max"))), stringsAsFactors=F,row.names = NULL)
 agentHistory2 <<- data.frame(matrix(vector(), 0, 3, dimnames=list(c(), c("iteration", "price", "max"))), stringsAsFactors=F,row.names = NULL)
 
-# create order book variable
-orderBook <<- data.frame(matrix(vector(), 0, 2, dimnames=list(c(), c("id", "price"))), stringsAsFactors=F,row.names = NULL)
+# read in simulation data
+readSimulationData()
 
-for (i in 1:5)
+agent1Win <<- c()
+agent2Win <<- c()
+
+
+
+# simulation function
+simulate <- function()
 {
-    cat(sprintf("%d-th round of bidding\n", i))
+    # create order book variable
+    orderBook <<- data.frame(matrix(vector(), 0, 2, dimnames=list(c(), c("id", "price"))), stringsAsFactors=F,row.names = NULL)
     
-    price1 <- readline(prompt = "Enter price for agent 1: ")
-    price1 <- as.numeric(price1)
-    #price1 <- zi(30, 50)
-    if(price1==-2)
-        break;
-    if(price1!=-1)
-        bid(i, 1, price1)
+    # simulation process
+    for (i in 1:2)
+    {
+        cat(sprintf("%d-th round of bidding\n", i))
+        
+        #price1 <- readline(prompt = "Enter price for agent 1: ")
+        #price1 <- as.numeric(price1)
+        price1 <- zi(30, 50)
+        #price1 <- agentGD(3)
+        
+        if(price1==-2)
+            break;
+        if(price1!=-1)
+            bid(i, 1, price1)
+        
+        #price2 <- readline(prompt = "Enter price for agent 2: ")
+        #price2 <- as.numeric(price2)
+        #print(price2)
+        price2 <- agentGD(1)
+        if(price2==-2)
+            break;
+        if(price2 != -1)
+            bid(i, 2, price2)
+    }
     
-    price2 <- readline(prompt = "Enter price for agent 2: ")
-    price2 <- as.numeric(price2)
-    print(price2)
-    if(price2==-2)
-        break;
-    if(price2 != -1)
-        bid(i, 2, price2)
+    if(orderBook$id[length(orderBook$price)] == 1)
+        agent1Win <<- c(agent1Win, orderBook$price[length(orderBook$price)])
+    else
+        agent2Win <<- c(agent2Win, orderBook$price[length(orderBook$price)])
+    
 }
 
-recordGameHistory()
+for (i in c(1:3000))
+    simulate()
+
+mean(agent1Win)
+mean(agent2Win)
+length(agent1Win)
+length(agent2Win)
+
+recordGameHistory() # careful: can not run with readline() well
